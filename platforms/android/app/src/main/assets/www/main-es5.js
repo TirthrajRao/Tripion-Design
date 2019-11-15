bc90bddddaf1aea74b93f4f296227f11b1710583
komalsakhiya <komalsakhiya21@gmail.com>
2019-11-15 12:52:41 +0530

    breifcase screen design

--- platforms/android/app/src/main/assets/www/main-es5.js
+++ platforms/android/app/src/main/assets/www/main-es5.js
@@ -674,7 +674,7 @@ Object(_angular_platform_browser_dynamic__WEBPACK_IMPORTED_MODULE_1__["platformB
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /home/rao/Downloads/tripion-design/src/main.ts */"./src/main.ts");
+module.exports = __webpack_require__(/*! /var/www/html/tripion_akshita/src/main.ts */"./src/main.ts");
 
 
 /***/ })
